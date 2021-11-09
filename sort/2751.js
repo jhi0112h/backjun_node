5be4a803aee11a26753c6e3b887dc3f28164c43f
johyeon-il <jhi0112h@gmail.com>
2021-11-10 03:16:04 +0900

    2751 clear

---
+++ sort/2751.js
@@ -0,0 +1,44 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let count = 0;
+let input = [];
+rl.on("line", function (line) {
+    if (count === 0) {
+        count = parseInt(line);
+    } else {
+        input.push(parseInt(line));
+    }
+
+    if (input.length === count) {
+        rl.close();
+    }
+}).on("close", function () {
+    let i = 0;
+    console.log(mergeSort(input).join("\n"));
+
+    process.exit();
+});
+
+function mergeSort(list) {
+    let len = list.length,
+        mid = Math.ceil(len / 2);
+
+    if (len === 1) return list;
+
+    return merge(mergeSort(list.slice(0, mid)), mergeSort(list.slice(mid)));
+}
+
+function merge(left, right) {
+    let result = [];
+    while (left.length !== 0 && right.length !== 0) {
+        left[0] <= right[0] ? result.push(left.shift()) : result.push(right.shift());
+    }
+    
+    if (left.length === 0) result.push(...right);
+    if (right.length === 0) result.push(...left);
+    return result;
+}

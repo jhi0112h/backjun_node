4db8a43c18ca1802c7d5034ab95f833e1bb2c2ff
johyeon-il <jhi0112h@gmail.com>
2021-11-12 14:58:18 +0900

    add quick sort algorism

--- sort/2751.js
+++ sort/2751.js
@@ -18,7 +18,8 @@ rl.on("line", function (line) {
     }
 }).on("close", function () {
     let i = 0;
-    console.log(mergeSort(input).join("\n"));
+    //console.log(mergeSort(input).join("\n"));
+    console.log(quickSort(input).join("\n"));
 
     process.exit();
 });
@@ -37,8 +38,40 @@ function merge(left, right) {
     while (left.length !== 0 && right.length !== 0) {
         left[0] <= right[0] ? result.push(left.shift()) : result.push(right.shift());
     }
-    
+
     if (left.length === 0) result.push(...right);
     if (right.length === 0) result.push(...left);
     return result;
+}
+
+function quickSort(array, left = 0, right = array.length - 1) {
+    if (left >= right) {
+        return;
+    }
+    const mid = Math.floor((left + right) / 2);
+    const pivot = array[mid];
+    const partition = divide(array, left, right, pivot);
+    quickSort(array, left, partition - 1);
+    quickSort(array, partition, right);
+
+    function divide(array, left, right, pivot) {
+        while (left <= right) {
+            while (array[left] < pivot) {
+                left++;
+            }
+            while (array[right] > pivot) {
+                right--;
+            }
+            
+            if (left <= right) {
+                let swap = array[left];
+                array[left] = array[right];
+                array[right] = swap;
+                left++;
+                right--;
+            }
+        }
+        return left;
+    }
+    return array;
 }
